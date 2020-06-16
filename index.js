5b3d8b5073f04f68a41ed394433c82064c6dda97
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-16 19:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (anArr){
+
+  let newObj = {
+    first:anArr[0],
+    second:anArr[1],
+  }
+
+  let
+  return `${anArr[0]}${anArr[1]}`
+
+
+}
